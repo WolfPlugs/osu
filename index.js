77e29634e198db67f3bdbbdc5fe254ce581e6874
Wolfie <Wolfkid200444@gmail.com>
2021-07-31 17:38:41 -0400

    lol

--- index.js
+++ index.js
@@ -2,42 +2,51 @@ const { Plugin } = require("powercord/entities");
 const { get } = require("powercord/http");
 
 module.exports = class Osu extends Plugin {
+
+  async sendBotMessage(content) {
+    const received = createBotMessage(channels.getChannelId(), content)
+    received.author.username = "Error Daddy"
+    return receiveMessage(received.channel_id, received)
+}
+
   startPlugin() {
     powercord.api.commands.registerCommand({
       command: "osu",
       description: "Get a user STATs from OSU",
       usage: "{c} [username]",
-      // send the user a message with the user stats
       executor: async (args) => {
         // get the user stats
         try {
-        const { body } = await get(`https://api.obamabot.ml/text/osu?user=${args.join(" ")}`);
-        if(body.length === 0) {
+          const { body } = await get(
+            `https://api.obamabot.ml/text/osu?user=${args.join(" ")}`
+          );
+          if (body.length === 0) {
+            this.sendBotMessage("No user found");
+            return
+          }
+          // string the stats
+          const string = [
+            `__${body.username}'s Stats__`,
+            `Global Rank: **${
+              body.formated_pp_rank
+            }** (:flag_${body.country.toLowerCase()}: #${
+              body.formated_pp_country_rank
+            })`,
+            `PP: **${body.pp_raw}**`,
+            `Play Count: **${body.playcount}**`,
+            `Accuracy: **${body.short_accuracy}%**`,
+            `Time Played: **${body.time_played}**`,
+          ].join("\n");
+
+          // send the user a message with the user stats
           return {
-            send: true, result:"No user found"
-          }
-        }
-        // string the stats
-        const string = [
-          `__${body.username}'s Stats__`,
-          `Global Rank: **${body.formated_pp_rank}** (:flag_${body.country.toLowerCase()}: #${body.formated_pp_country_rank})`,
-          `PP: **${body.pp_raw}**`,
-          `Play Count: **${body.playcount}**`,
-          `Accuracy: **${body.short_accuracy}%**`,
-          `Time Played: **${body.time_played}**`,
-      ].join('\n')
-
-        // send the user a message with the user stats
-        return {
-          send: true,
-          result: string,
-        }
-
+            send: true,
+            result: string,
+          };
         } catch (e) {
-          return  {
-            send: true, result: `Error:  ${e}`
-          }
-        }        
+          this.sendBotMessage(e.message);
+          return
+        }
       },
     });
   }
@@ -45,5 +54,4 @@ module.exports = class Osu extends Plugin {
   pluginWillUnload() {
     powercord.api.commands.unregisterCommand("osu");
   }
-  
 };
